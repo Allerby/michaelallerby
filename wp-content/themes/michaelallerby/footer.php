4e739d364b0e1b286ac718ca9f2ec505c347d7bf
Michael Lee Allerby <michael.allerby@designworks.co.nz>
2014-10-21 08:17:55 +1300

    Initial Commit

---
+++ wp-content/themes/michaelallerby/footer.php
@@ -0,0 +1,18 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <ul class="legals">
+          <li><a href="#">Terms &amp; Conditions</a></li>
+          <li><a href="#">Legals</a></li>
+        </ul>
+      </div>
+      <div class="col-md-6 credit">
+        <p>Designed &amp; Developed by <a href="http://www.peterfinlan.com/">Peter Finlan</a> exclusively for <a href="http://tympanus.net/codrops/"><em>Codrops</em></a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
